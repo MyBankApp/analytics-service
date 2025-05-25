99c92699ab0e0827df489138fab77d48b2d45e5d
Topinambur02 <alext0408@yandex.ru>
2025-05-25 18:27:28 +0300

    #5 feat: add distribution for period

--- service/analyticService.py
+++ service/analyticService.py
@@ -7,8 +7,26 @@ class AnalyticService:
         transactions = await transaction_service.get_transactions_by_sender_id(sender_id)
         dataframe = pd.DataFrame(transactions)
         dataframe['category_name'] = dataframe['category'].apply(lambda x: x.get('name'))
-        category_distribution = dataframe.groupby('category_name')['amount'].sum().sort_values(ascending=False).to_dict()
+        category_distribution = dataframe \
+                                    .groupby('category_name')['amount'] \
+                                    .sum() \
+                                    .sort_values(ascending=False) \
+                                    .to_dict()
 
         return category_distribution
+    
+    async def distribution_for_period(self, sender_id: int):
+        transactions = await transaction_service.get_transactions_by_sender_id(sender_id)
+        dataframe = pd.DataFrame(transactions)
+        dataframe['date'] = pd.to_datetime(dataframe['createdAt']).dt.date
+        distribution_for_period = (
+            dataframe
+                .groupby('date')['amount']
+                .sum()
+                .sort_values(ascending=False)
+                .to_dict()
+        )
+
+        return distribution_for_period
 
 service = AnalyticService()
