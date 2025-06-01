20e9bc13bbe541d8dc28354e5c0414a5cd934336
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-06-01 16:29:15 +0300

    #7 feat: add transformation data and remove data for bar chart

--- controller/analyticController.py
+++ controller/analyticController.py
@@ -6,7 +6,3 @@ analytic_controller = APIRouter()
 @analytic_controller.get("/category/{sender_id}")
 async def category_distribution(sender_id: int):
     return await service.category_distribution(sender_id)
-
-@analytic_controller.get("/period/{sender_id}")
-async def distribution_for_period(sender_id: int):
-    return await service.distribution_for_period(sender_id)
