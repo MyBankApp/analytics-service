0a75b29d1533192dd6ce58a76cd1055d69241a19
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-06-01 18:48:47 +0300

    #9 feat: add pdf transaction report generator

--- apiRouter.py
+++ apiRouter.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from controller.transactionController import transaction_controller
 from controller.analyticController import analytic_controller
+from controller.reportController import reportController
 
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(transaction_controller, prefix="/transactions")
 api_router.include_router(analytic_controller, prefix="/analytic")
+api_router.include_router(reportController, prefix="/report")
